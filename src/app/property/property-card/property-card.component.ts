b7686321e247dafd0667b5bc10ddf48e8d3a14ef
Shubham Jain <shubhamjainnie@gmail.com>
2024-01-15 13:58:08 +0530

    Code changes

--- src/app/property/property-card/property-card.component.ts
+++ src/app/property/property-card/property-card.component.ts
@@ -6,5 +6,10 @@ import { Component } from "@angular/core";
   styleUrls: ['property-card.component.css']
 })
 export class PropertyCardComponent{
-
+  Property: any = {
+    "Id":1,
+    "Name":"Shalom",
+    "Type":"House",
+    "Price":12000
+  }
 }
